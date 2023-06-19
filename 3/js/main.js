7eb53ac0fc409d4f4f6f9cf4bfb6a95028882d5d
Keks <keks@htmlacademy.ru>
2023-06-19 20:50:28 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,74 @@
+const PHOTO_DESCRIPTIONS_COUNT = 25;
+let photoId = 1;
+let commentId = 1;
+
+const AVATAR_COUNTER = {
+  min: 1,
+  max: 6
+};
+
+const LIKES_COUNTER = {
+  min: 15,
+  max: 200
+};
+
+const COMMENTS_COUNTER = {
+  min: 1,
+  max: 30
+};
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Анна Полянская',
+  'Саша Пупсикова',
+  'Александр Демидов',
+  'Ксения Игоревна',
+  'София Горбатова',
+  'Марина Сергеевна',
+  'Алиса Соболева',
+  'Андрей Меньшенин',
+  'Ася Харитонова',
+  'Алина Горбатова',
+];
+
+const DESCRIPTIONS = [
+  'Никогда не обижай своих близких.',
+  'Хорошие люди существуют!',
+  'Не разлей вода.',
+  'С косметикой надо быть аккуратней.',
+  'Нет предела совершенству',
+  'Это здорово.'
+];
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomArrayElement = (elements) => elements[getRandomInteger (0, elements.length - 1)];
+const createMessage = () => {
+  const message = Array.from({ length: getRandomInteger(1, 2) }, () => getRandomArrayElement(MESSAGES));
+  return Array.from(new Set(message)).join(' ');
+};
+
+const createCommentsArray = () => ({
+  id: commentId++,
+  avatar:  `img/avatar-${getRandomInteger(AVATAR_COUNTER.min, AVATAR_COUNTER.max)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createDescription = () => ({
+  id: photoId,
+  url: `photos/${photoId++}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LIKES_COUNTER.min, LIKES_COUNTER.max),
+  comments: Array.from({length: getRandomInteger(COMMENTS_COUNTER.min, COMMENTS_COUNTER.max)}, createCommentsArray)
+});
+
+const createPhotoDescriptions = () => Array.from({length: PHOTO_DESCRIPTIONS_COUNT}, createDescription);
+
+createPhotoDescriptions();
