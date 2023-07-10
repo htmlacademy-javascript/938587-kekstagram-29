b0cdf80525620ce0e3ca9b5b0d2e1ff0f1bb12a1
Keks <keks@htmlacademy.ru>
2023-07-10 22:32:34 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/display-thumbnails.js
@@ -0,0 +1,33 @@
+import { displayBigPicture } from './display-big-picture.js';
+import { createPhotoDescriptions } from './create-photo-descriptions.js';
+
+const descriptionsList = createPhotoDescriptions();
+const thumbnailsListFragment = document.createDocumentFragment();
+const picturesList = document.querySelector('.pictures');
+const photoThumbnailTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const createThumbnail = (description) => {
+  const photoThumbnail = photoThumbnailTemplate.cloneNode(true);
+  const imageTemplate = photoThumbnail.querySelector('.picture__img');
+
+  imageTemplate.src = description.url;
+  imageTemplate.alt = description.description;
+  photoThumbnail.querySelector('.picture__likes').textContent = description.likes;
+  photoThumbnail.querySelector('.picture__comments').textContent = description.comments.length;
+
+  photoThumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    displayBigPicture(description);
+  });
+
+  return photoThumbnail;
+};
+
+const displayThumbnails = () => {
+  descriptionsList.forEach((item) => thumbnailsListFragment.append(createThumbnail(item)));
+  picturesList.append(thumbnailsListFragment);
+};
+
+export { displayThumbnails };
---
+++ 8/js/main.js
@@ -0,0 +1,3 @@
+import {displayThumbnails} from './display-thumbnails.js';
+
+displayThumbnails();
